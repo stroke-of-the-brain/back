8cfa3762551738818361e45964dd9a1e99be31a1
허세진 <heosejin222@gmail.com>
2025-03-05 00:02:26 +0900

    STT 변환 에러

--- src/main/java/com/naebom/stroke/naebom/config/SpeechConfig.java
+++ src/main/java/com/naebom/stroke/naebom/config/SpeechConfig.java
@@ -3,14 +3,25 @@ package com.naebom.stroke.naebom.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class SpeechConfig {
 
+    private static final String CREDENTIALS_PATH = "/Users/heosejin222gmail.com/Desktop/내봄/naebom 2/src/main/resources/google-cloud-key.json";
+
     @Bean
     public GoogleCredentials googleCredentials() throws IOException {
-        return GoogleCredentials.fromStream(new FileInputStream("src/main/resources/google-cloud-key.json"));
+        File credentialsFile = new File(CREDENTIALS_PATH);
+
+        if (!credentialsFile.exists()) {
+            throw new IOException("Google Cloud 인증 키 파일이 존재하지 않습니다: " + CREDENTIALS_PATH);
+        }
+
+        System.out.println("Google Cloud 인증 키 로드 성공! 경로: " + CREDENTIALS_PATH);
+        return GoogleCredentials.fromStream(new FileInputStream(credentialsFile));
     }
 }
